963a9f5ffa79383fa79d31747714fbc24d4685bb
Justin Jinsuk Kim <jinsuk@Justins-MacBook-Air.local>
2020-02-21 10:51:21 -0600

    added state, routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
-function App() {
-	return (
-		<div className="App">
-			<p>hello</p>
-		</div>
-	);
+import Nav from 'react-bootstrap/Nav';
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: 'Justin Kim',
+			headerLinks: [
+				{ title: 'Home', path: '/' },
+				{ title: 'About', path: '/about' },
+				{ title: 'Contact', path: '/contact' }
+			],
+			home: {
+				title: 'Persevere',
+				subTitle: 'Projects that make a difference',
+				text: 'Checkout my projects below'
+			},
+			about: {
+				title: 'About Me'
+			},
+			contact: {
+				title: "Let's Talk!"
+			}
+		};
+	}
+	render() {
+		return (
+			<Router>
+				<Container className="p-0" fluid={true}>
+					<Navbar className="border-bottom" bg="transparent" expand="lg">
+						<Navbar.Brand>Justin Kim</Navbar.Brand>
+						<Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+						<Navbar.Collapse id="navbar-toggle">
+							<Nav className="ml-auto">
+								<Link className="nav-link" to="/">
+									Home
+								</Link>
+								<Link className="nav-link" to="/about">
+									About
+								</Link>
+								<Link className="nav-link" to="/contact">
+									Contact
+								</Link>
+							</Nav>
+						</Navbar.Collapse>
+					</Navbar>
+				</Container>
+			</Router>
+		);
+	}
 }
 
 export default App;
